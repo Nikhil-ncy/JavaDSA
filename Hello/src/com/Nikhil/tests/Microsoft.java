3109ec2dab1f6213eed8e21c41b60780bfa2bc69
Nikhil-ncy <nikhilchandra9455@gmail.com>
2024-02-24 14:53:02 +0530

    first commit

---
+++ Hello/src/com/Nikhil/tests/Microsoft.java
@@ -0,0 +1,11 @@
+package com.Nikhil.tests;
+import java.io.*;
+
+
+public class Microsoft {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String name = br.readLine();
+        System.out.println(name);
+    }
+}
